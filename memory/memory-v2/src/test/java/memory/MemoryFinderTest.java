5c85e0fd7e9b207dc78a1eaa4b1fedbd0f562133
qwe5507 <apvmffkdls@gmail.com>
2023-06-12 21:14:19 +0900

    자동 구성 라이브러리 만들기

---
+++ memory/memory-v2/src/test/java/memory/MemoryFinderTest.java
@@ -0,0 +1,18 @@
+package memory;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MemoryFinderTest {
+
+    @Test
+    void get() {
+        MemoryFinder memoryFinder = new MemoryFinder();
+        Memory memory = memoryFinder.get();
+        System.out.println("memory = " + memory);
+        Assertions.assertThat(memory).isNotNull();
+    }
+
+}
